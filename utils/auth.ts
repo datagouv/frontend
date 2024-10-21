0e07113a65d8dbe132fbdfbf7c2d4f1d76b0179c
Thibaud Dauce <git@hadibut.fr>
2024-10-21 15:22:35 +0200

    Add correct header for logged-in user

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,14 +1,15 @@
+import type { User } from "@datagouv/components";
 import { get } from "./api"
 import { onMounted } from "vue";
 
-const me = ref(null);
+const me = ref<User | null>(null);
 
 export const reloadAuth = async () => {
     const response = await get('http://dev.local:7000/api/1/me')
     me.value = response.data
 }
 
-export const useMe = () => {
+export const useMe = (): Ref<User | null> => {
     onMounted(async () => {
         reloadAuth()
     })
