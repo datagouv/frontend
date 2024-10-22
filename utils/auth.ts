1065b3080f35b6d0eb8d27931914ff6497133421
Thibaud Dauce <git@hadibut.fr>
2024-10-22 18:08:57 +0200

    Remove logs

--- utils/auth.ts
+++ utils/auth.ts
@@ -19,7 +19,6 @@ export const refreshMe = async (meState: Ref<User | null>) => {
     // we don't want the classic error management that redirect
     // to the login page when a 401 is raised. So we must manually
     // re-configured the baseURL.
-    console.log('calling refresh me!')
     const config = useRuntimeConfig();
     const cookie = useRequestHeader('cookie');
 
@@ -28,11 +27,11 @@ export const refreshMe = async (meState: Ref<User | null>) => {
     let headers: Record<string, string> = {};
 
     if (cookie) {
-        console.log('Cookie is set to ' + cookie)
+        // console.log('Cookie is set to ' + cookie)
         headers['cookie'] = cookie
     }
     if (token.value) {
-        console.log('Token is set to ' + token.value)
+        // console.log('Token is set to ' + token.value)
         headers['Authentication-Token'] = token.value
     }
 
@@ -43,7 +42,6 @@ export const refreshMe = async (meState: Ref<User | null>) => {
             headers,
         })
     } catch (e) {
-        console.error(e)
         meState.value = null
     }
 }
