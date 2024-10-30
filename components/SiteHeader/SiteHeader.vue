6749e30e031c81aa8838592722de448f3aca7e19
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-10-30 16:35:28 +0100

    Redirect admin to page (#42)
    * fix: current selected page
    
    * fix: header fluid
    
    * fix: redirect to page
    
    * refac: use navigateTo

--- components/SiteHeader/SiteHeader.vue
+++ components/SiteHeader/SiteHeader.vue
@@ -6,7 +6,7 @@
     <div class="fr-header__body">
       <div
         class="fr-container"
-        :class="{ 'max-w-none overflow-hidden': fluid }"
+        :class="{ '!max-w-none !overflow-hidden': fluid }"
       >
         <div class="fr-header__body-row">
           <div class="fr-header__brand fr-enlarge-link">
@@ -200,7 +200,7 @@
     >
       <div
         class="fr-container"
-        :class="{ 'max-w-none overflow-hidden': fluid }"
+        :class="{ '!max-w-none !overflow-hidden': fluid }"
       >
         <nav
           class="fr-nav"
