1bcd3c3335b6660f6b65213195d261d42f220fd7
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-11-12 16:20:37 +0100

    fix: tooltip (#90)

--- components/Tooltip/Tooltip.vue
+++ components/Tooltip/Tooltip.vue
@@ -1,20 +1,31 @@
 <template>
-  <p
-    v-bind="$attrs"
-    :aria-describedby="id"
+  <div
+    class="relative"
+    @focusin="show = true"
+    @mouseenter="show = true"
+    @focusout="show = false"
+    @mouseleave="show = false"
   >
-    <slot />
-  </p>
-  <span
-    :id="id"
-    class="fr-tooltip fr-placement"
-    role="tooltip"
-    aria-hidden="true"
-  >
-    <slot name="tooltip" />
-  </span>
+    <p
+      v-bind="$attrs"
+      :aria-describedby="id"
+    >
+      <slot />
+    </p>
+    <div
+      v-show="show"
+      :id
+      role="tooltip"
+      aria-hidden="true"
+      class="absolute drop-shadow bg-white p-2 left-1/2 z-10 mt-0 w-screen max-w-56 -translate-x-1/2 transform"
+      static
+    >
+      <slot name="tooltip" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 const id = useId()
+const show = ref(false)
 </script>
