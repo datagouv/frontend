73ef24e081d5cc73c46272d4461d11ccad4e122f
maudetes <maudet.estelle@gmail.com>
2024-11-18 16:32:00 +0100

    Trim scheme from environment in sentry config

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -6,7 +6,7 @@ dotenv.config()
 Sentry.init({
   // We can't read useRuntimeConfig here, reading configs from .env
   dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.NUXT_PUBLIC_API_BASE,
+  environment: process.env.NUXT_PUBLIC_API_BASE?.replace(/https?:\/\//, ''),
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
