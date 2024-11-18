3494f49a06d857f808a0c164836688b4244074a9
maudetes <maudet.estelle@gmail.com>
2024-11-18 15:51:29 +0100

    Add sentry integration and configuration (#146)
    Followed https://docs.sentry.io/platforms/javascript/guides/nuxt/.
    Succeeded to send errors to our sentry.
    
    We can add [stack traces using site
    maps](https://docs.sentry.io/platforms/javascript/guides/nuxt/#add-readable-stack-traces-to-errors)
    later on since I could not make it work right away.
    
    `NUXT_PUBLIC_SENTRY_DSN` should be added on our infra configuration to
    override the nuxt config and be used by the sentry server configuration.

---
+++ sentry.server.config.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/nuxt";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+Sentry.init({
+  // We can't read useRuntimeConfig here, reading configs from .env
+  dsn: process.env.NUXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.NUXT_PUBLIC_API_BASE,
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 0.1,
+});
