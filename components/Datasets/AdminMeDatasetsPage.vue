dbe939126de7fa142d042beafa70429e19a79d06
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-11-19 11:51:45 +0100

    fix: admin me page

---
+++ components/Datasets/AdminMeDatasetsPage.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <Breadcrumb>
+      <li>
+        <NuxtLinkLocale
+          class="fr-breadcrumb__link"
+          to="/beta/admin"
+        >
+          {{ t('Administration') }}
+        </NuxtLinkLocale>
+      </li>
+      <li>
+        <a
+          class="fr-breadcrumb__link"
+          aria-current="page"
+        >
+          {{ t('Datasets') }}
+        </a>
+      </li>
+    </Breadcrumb>
+    <h1 class="fr-h3 fr-mb-5v">
+      {{ t("Datasets") }}
+    </h1>
+
+    <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--middle">
+      <div class="fr-col">
+        <h2
+          v-if="status === 'success' && pageData.length"
+          class="subtitle subtitle--uppercase fr-m-0"
+        >
+          {{ t('{n} datasets', pageData.length) }}
+        </h2>
+      </div>
+    </div>
+    <AdminDatasetsTable
+      v-if="status === 'pending' || (status === 'success' && pageData.length > 0)"
+      :datasets="pageData ? pageData : []"
+      :loading="status === 'pending'"
+    />
+    <div
+      v-else
+      class="flex flex-col items-center"
+    >
+      <nuxt-img
+        src="/illustrations/dataset.svg"
+        class="h-20"
+      />
+      <p class="fr-text--bold fr-my-3v">
+        {{ t(`You haven't published a dataset yet`) }}
+      </p>
+      <AdminPublishButton type="dataset" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Dataset } from '@datagouv/components'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import Breadcrumb from '../Breadcrumb/Breadcrumb.vue'
+import AdminDatasetsTable from '~/components/AdminTable/AdminDatasetsTable/AdminDatasetsTable.vue'
+
+const { t } = useI18n()
+const config = useRuntimeConfig()
+
+const url = computed(() => {
+  const url = new URL(`/api/1/me/datasets/`, config.public.apiBase)
+  return url.toString()
+})
+
+const { data: pageData, status } = await useAPI<Array<Dataset>>(url, { lazy: true })
+</script>
