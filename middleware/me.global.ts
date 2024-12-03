3e333420456607edf98b9770a5174d65a38d2636
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-12-03 09:25:19 +0100

    feat: prerender static pages

--- middleware/me.global.ts
+++ middleware/me.global.ts
@@ -1,4 +1,4 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware(async (_to, _from) => {
   // console.log(`Calling me middleware ${from.path} -> ${to.path}`)
   const me = useMaybeMe()
   if (me.value !== undefined) {
