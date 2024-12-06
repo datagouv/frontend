276b039e6786caf2c14b8233ce1ea8eab8a1e64e
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-12-06 09:43:44 +0100

    fix: remove duplicated section and add links

--- pages/design.vue
+++ pages/design.vue
@@ -81,24 +81,31 @@
 
         <BannerNotif />
       </section>
-
-      <section>
-        <h2 class="!mb-3">
-          BannerNotif
-        </h2>
-
-        <BannerNotif />
-      </section>
     </div>
     <div class="space-y-8 py-8 pb-64">
       <h2 class="!mb-3">
         Full width content
       </h2>
 
-      <section>
+      <section class="space-y-3">
         <h3 class="!mb-3">
           BannerSticky in a contained page
         </h3>
+
+        <div class="flex gap-3">
+          <NuxtLinkLocale
+            :external="true"
+            to="https://www.figma.com/design/fvtAhJSd1hURLxy8GXmc3k/%F0%9F%8E%81-%5BWIP%5D-Syt%C3%A8me-U?node-id=105-4173&t=5JmPave6D8hPb2gG-4"
+          >
+            View design
+          </NuxtLinkLocale>
+          <NuxtLinkLocale
+            :external="true"
+            to="https://github.com/datagouv/front-end/blob/main/components/BannerSticky.vue"
+          >
+            View code
+          </NuxtLinkLocale>
+        </div>
 
         <DesignSystemBannerSticky />
       </section>
