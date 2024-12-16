6c93d1a2b03742b491b6cd011d3aa461fb1c099f
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-12-16 10:28:28 +0100

    Add harvester blank state (#202)

--- components/AdminPublishButton/AdminPublishButton.vue
+++ components/AdminPublishButton/AdminPublishButton.vue
@@ -2,9 +2,11 @@
   <NuxtLinkLocale
     v-if="!config.public.readOnlyMode"
     :to="link"
+    :external="type === 'harvester'"
     class="fr-btn fr-icon-add-line fr-btn--icon-left"
   >
-    <span>{{ t("Publish on") }}</span>
+    <span v-if="type === 'harvester'">{{ t("Create an harvester on") }}</span>
+    <span v-else>{{ t("Publish on") }}</span>
     <SiteLogo
       class="ml-1.5 mt-0.5 h-4"
       white
@@ -29,8 +31,7 @@ const link = computed(() => {
     case 'dataset':
       return '/beta/admin/datasets/new'
     case 'harvester':
-      return ''
-      // return `${admin_root}harvester/new/`;
+      return `/admin/harvester/new/`
     case 'reuse':
       return '/beta/admin/reuses/new'
     case 'dataservice':
