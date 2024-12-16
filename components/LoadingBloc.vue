d038da88d852fa09b63ff390c4cf03a08165ac99
Thibaud Dauce <git@hadibut.fr>
2024-12-16 09:18:31 +0100

    Attempt to improve loading state

---
+++ components/LoadingBloc.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative">
+    <div :class="{ 'opacity-50': loading }">
+      <slot />
+    </div>
+    <div
+      v-if="loading"
+      class="absolute inset-0 flex justify-center items-center "
+    >
+      <AdminLoader class="size-32" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  status: string
+}>()
+
+const loading = computed(() => {
+  return props.status === 'idle' || props.status === 'pending'
+})
+</script>
