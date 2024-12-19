15b44e7cb2ddf6a50352b25470160a8749c63a6d
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-12-19 15:18:33 +0100

    feat: wip footer

--- components/SiteFooter/SiteFooter.vue
+++ components/SiteFooter/SiteFooter.vue
@@ -1,16 +1,16 @@
 <template>
-  <!-- <footer role="contentinfo">
+  <footer role="contentinfo">
     <div class="fr-follow">
       <div class="fr-container">
         <div class="fr-grid-row">
           <div class="fr-col-12 fr-col-md-8 flex">
             <div class="fr-col-auto fr-mr-5v">
-              <img
+              <NuxtImg
                 src="/img/newsletter.svg"
                 loading="lazy"
                 width="71"
                 height="55"
-              >
+              />
             </div>
             <div class="fr-follow__newsletter fr-col">
               <div>
@@ -20,25 +20,27 @@
                 <p class="fr-text--lg fr-text--alt f-italic">
                   {{
                     $t("To stay up to date about {site} and open data news, subscribe to our newsletter and follow our events.",
-                       { site: title })
+                       { site: config.public.title })
                   }}
                 </p>
               </div>
               <div>
-                <a
+                <NuxtLinkLocale
                   class="fr-btn fr-mr-2w fr-my-1w"
                   :title="$t('Subscribe to our newsletter')"
-                  href="{{ config.NEWSLETTER_SUBSCRIPTION_URL }}"
+                  :to="config.public.newsletterSubscriptionUrl"
+                  :external="true"
                 >
                   {{ $t("Subscribe") }}
-                </a>
-                <a
+                </NuxtLinkLocale>
+                <NuxtLinkLocale
                   class="fr-btn fr-btn--secondary fr-btn--secondary-grey-500"
                   :title="$t('Subscribe to our newsletter')"
-                  href="{{ url_for('gouvfr.show_page', slug='webinaires') }}"
+                  to="/pages/webinaires/"
+                  :external="true"
                 >
                   {{ $t("See our next events") }}
-                </a>
+                </NuxtLinkLocale>
               </div>
             </div>
           </div>
@@ -56,10 +58,7 @@
                     href="https://twitter.com/datagouvfr"
                     target="_blank"
                   >
-                    <span
-                      class="fr-icon-twitter-line fr-mr-1w"
-                      aria-hidden="true"
-                    /> Twitter
+                    <RiTwitterXLine />
                   </a>
                 </li>
                 <li>
@@ -69,11 +68,7 @@
                     href="https://www.linkedin.com/company/data-gouv-fr/"
                     target="_blank"
                   >
-                    <span
-                      class="fr-icon-linkedin-box-line fr-mr-1w"
-                      aria-hidden="true"
-                    />
-                    Linkedin
+                    <RiLinkedinLine />
                   </a>
                 </li>
                 <li>
@@ -83,38 +78,29 @@
                     href="https://social.numerique.gouv.fr/@datagouvfr"
                     target="_blank"
                   >
-                    <span
-                      class="fr-icon-mastodon-line fr-mr-1w"
-                      aria-hidden="true"
-                    />
-                    Mastodon
+                    <RiMastodonLine />
                   </a>
                 </li>
                 <li>
                   <a
                     class="fr-link"
                     :title="$t('Github - new tab')"
-                    href="https://github.com/etalab/data.gouv.fr"
+                    href="https://github.com/datagouv/data.gouv.fr"
                     target="_blank"
                   >
-                    <span
-                      class="fr-icon-github-line fr-mr-1w"
-                      aria-hidden="true"
-                    /> Github
+                    <RiGithubLine />
                   </a>
                 </li>
                 <li>
-                  <a
+                  <NuxtLinkLocale
                     class="fr-link"
                     :title="$t('RSS - new tab')"
-                    href="{{ url_for('datasets.recent_feed') }}"
+                    to="datasets/recent.atom"
+                    :external="true"
                     target="_blank"
                   >
-                    <span
-                      class="fr-icon-rss-line fr-mr-1w"
-                      aria-hidden="true"
-                    /> RSS
-                  </a>
+                    <RiRssLine />
+                  </NuxtLinkLocale>
                 </li>
               </ul>
             </div>
@@ -134,14 +120,15 @@
                 {{ $t('Open Data') }}
               </h3>
               <ul class="fr-footer__top-list">
-                {% for item in nav.gouvfr_opendata %}
-                <li>
+                <li
+                  v-for="item in openDataLinks"
+                  :key="item.label"
+                >
                   <a
                     class="fr-footer__top-link"
-                    :href="item.url"
+                    :href="item.link"
                   >{{ item.label }}</a>
                 </li>
-                {% endfor %}
               </ul>
             </div>
             <div class="fr-col-12 fr-col-sm-3">
@@ -149,14 +136,15 @@
                 {{ $t('Platform') }}
               </h3>
               <ul class="fr-footer__top-list">
-                {% for item in nav.gouvfr_platform %}
-                <li>
+                <li
+                  v-for="item in platformLinks"
+                  :key="item.label"
+                >
                   <a
                     class="fr-footer__top-link"
-                    :href="item.url"
+                    :href="item.link"
                   >{{ item.label }}</a>
                 </li>
-                {% endfor %}
               </ul>
             </div>
             <div class="fr-col-12 fr-col-sm-3">
@@ -164,14 +152,15 @@
                 {{ $t('Resources') }}
               </h3>
               <ul class="fr-footer__top-list">
-                {% for item in nav.gouvfr_resources %}
-                <li>
+                <li
+                  v-for="item in resourcesLinks"
+                  :key="item.label"
+                >
                   <a
                     class="fr-footer__top-link"
-                    :href="item.url"
+                    :href="item.link"
                   >{{ item.label }}</a>
                 </li>
-                {% endfor %}
               </ul>
             </div>
             <div class="fr-col-12 fr-col-sm-3">
@@ -181,7 +170,7 @@
               <ul class="fr-footer__top-list">
                 <li>
                   <a
-                    :href="config.API_DOC_EXTERNAL_LINK"
+                    :href="config.public.apiDocExternalLink"
                     class="fr-footer__top-link"
                   >
                     {{ $t("Portal's API") }}
@@ -192,16 +181,16 @@
                     href="https://github.com/opendatateam/udata/"
                     class="fr-footer__top-link"
                   >
-                    {{ $t('Open-source engine: udata ({version})', { version: package_version('udata') })}}
+                    {{ $t('Open-source engine: udata') }}
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://github.com/datagouv/udata-front/"
-                    title="{{ _('data.gouv.fr extension: udata-front') }}"
+                    href="https://github.com/datagouv/front-end/"
+                    :title="$t('data.gouv.fr extension: front-end')"
                     class="fr-footer__top-link"
                   >
-                    {{ $t('udata theme extension: udata-front ({version})', { version: package_version('udata-front') }) }}
+                    {{ $t('data.gouv.fr front-end') }}
                   </a>
                 </li>
               </ul>
@@ -209,11 +198,8 @@
           </div>
         </div>
       </div>
-      {% endcache %}
       <div class="fr-container">
         <div class="fr-footer__body">
-          {% cache cache_duration, 'footer-brand-and-links',
-          g.lang_code|default(config.DEFAULT_LANGUAGE)|string %}
           <div class="fr-footer__brand fr-enlarge-link">
             <p
               class="fr-logo"
@@ -227,7 +213,7 @@
               href="/"
               :title="$t('Go back to website home - data.gouv.fr - République Française')"
             >
-              {% include theme('svg/logotype.svg') %}
+              <SiteLogo />
             </a>
           </div>
           <div class="fr-footer__content">
@@ -235,21 +221,21 @@
               {{ $t("A product of the Direction Interministérielle du Numérique(DINUM).") }}
             </p>
             <ul class="fr-footer__content-list">
-              {% for item in nav.gouvfr_network %}
-              <li class="fr-footer__content-item">
+              <li
+                v-for="item in networkLinks"
+                :key="item.label"
+                class="fr-footer__content-item"
+              >
                 <a
                   class="fr-footer__content-link"
                   target="_blank"
-                  :href="item.url"
-                >{{ item.label
-                }}</a>
+                  :href="item.link"
+                >{{ item.label }}</a>
               </li>
-              {% endfor %}
             </ul>
           </div>
-          {% endcache %}
           <div class="fr-col-auto fr-ml-auto self-end">
-            <nav
+            <!-- <nav
               role="navigation"
               class="fr-translate fr-nav"
             >
@@ -284,21 +270,24 @@
                   </ul>
                 </div>
               </div>
-            </nav>
+            </nav> -->
           </div>
         </div>
         <div class="fr-footer__bottom">
           <ul class="fr-footer__bottom-list">
-            {% for item in nav.gouvfr_footer %}
-            <li class="fr-footer__bottom-item">
-              <a
-                :href="'' if item.items else item.url"
+            <li
+              v-for="item in footerLinks"
+              :key="item.label"
+              class="fr-footer__bottom-item"
+            >
+              <NuxtLinkLocale
+                :to="item.link"
+                :external="true"
                 class="fr-footer__bottom-link"
               >
                 {{ item.label }}
-              </a>
+              </NuxtLinkLocale>
             </li>
-            {% endfor %}
           </ul>
           <div class="fr-footer__bottom-copy">
             <p>
@@ -307,12 +296,62 @@
             </p>
           </div>
         </div>
-        {% endcache %}
       </div>
     </div>
-  </footer> -->
+  </footer>
 </template>
 
 <script setup lang="ts">
+import { RiGithubLine, RiLinkedinLine, RiMastodonLine, RiRssLine, RiTwitterXLine } from '@remixicon/vue'
 
+const config = useRuntimeConfig()
+
+const { t } = useI18n()
+
+type Link = {
+  label: string
+  link: string
+}
+
+const openDataLinks: Array<Link> = [
+  { label: t('Topics'), link: '/pages/thematiques-a-la-une' },
+  { label: t('Reference Data'), link: 'pages/spd/reference' },
+]
+
+if (config.public.csvDatasetId) {
+  openDataLinks.push({ label: t('Data catalog'), link: `/datasets/${config.public.csvDatasetId}` })
+}
+
+openDataLinks.push({ label: t('Follow data opening'), link: 'https://ouverture.data.gouv.fr' })
+openDataLinks.push({ label: t('Portal for European data'), link: 'https://data.europa.eu' })
+
+const platformLinks: Array<Link> = [
+  { label: t('Guides'), link: config.public.guidesUrl },
+  { label: t('Roadmap and news'), link: '/pages/roadmap' },
+  { label: t('Contact us'), link: config.public.supportUrl },
+  { label: t('Give us your feedback'), link: config.public.feedbackFormUrl },
+  { label: t('Statistics'), link: '/dashboard' },
+]
+
+const resourcesLinks: Array<Link> = [
+  { label: 'api.gouv.fr', link: 'https://api.gouv.fr' },
+  { label: config.public.schemasSite.name, link: config.public.schemasSite.url },
+  { label: 'meteo.data.gouv.fr', link: 'https://meteo.data.gouv.fr' },
+  { label: 'transport.data.gouv.fr', link: 'https://transport.data.gouv.fr' },
+  { label: 'defis.data.gouv.fr', link: 'https://defis.data.gouv.fr' },
+]
+
+const footerLinks: Array<Link> = [
+  { label: t('Licences'), link: '/pages/legal/licences' },
+  { label: t('Terms of use'), link: '/pages/legal/cgu' },
+  { label: t('Tracking and privacy'), link: '/pages/gouvfr.suivi' },
+  { label: t('Legal notice'), link: '/pages/legal/legal-notice' },
+  { label: t('Accessibility: partially compliant'), link: '/pagelegal/accessibility' },
+]
+
+const networkLinks: Array<Link> = [
+  { label: 'legifrance.gouv.fr', link: 'https://legifrance.gouv.fr' },
+  { label: 'info.gouv.fr', link: 'https://www.info.gouv.fr' },
+  { label: 'service-public.fr', link: 'https://www.service-public.fr' },
+]
 </script>
