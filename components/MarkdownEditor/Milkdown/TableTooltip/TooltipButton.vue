edffcc2cfaba86082f883f3b0286a48e4bce8d5a
Thibaud Dauce <git@hadibut.fr>
2025-02-03 16:44:45 +0100

    Fix missing button type for markdown editor

--- components/MarkdownEditor/Milkdown/TableTooltip/TooltipButton.vue
+++ components/MarkdownEditor/Milkdown/TableTooltip/TooltipButton.vue
@@ -1,6 +1,7 @@
 <template>
   <button
     class="rounded-full p-2 border-solid border border-neutral-400 text-xs w-8 h-8 leading-none text-neutral-800 !bg-neutral-50 hover:!bg-neutral-200 flex items-center justify-center"
+    type="button"
     @click="emit('click')"
   >
     <component :is="icon" />
