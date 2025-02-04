7d7b48f440c33de066e73bee0cc8eba61dbdc52f
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-02-04 09:18:56 +0100

    fix: ssr

--- components/AdminContentWithTooltip/AdminContentWithTooltip.vue
+++ components/AdminContentWithTooltip/AdminContentWithTooltip.vue
@@ -1,30 +1,29 @@
 <template>
-  <p v-bind="$attrs" :aria-describedby="id" ref="contentRef">
-    <slot></slot>
-  </p>
-  <span class="fr-hidden-xl fr-tooltip fr-placement" :id="id" role="tooltip" aria-hidden="true">
-    {{ tooltipText }}
-  </span>
+  <Tooltip tooltip-class="xl:hidden">
+    <slot />
+    <template #tooltip>
+      {{ tooltipText }}
+    </template>
+  </Tooltip>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
 defineOptions({
   inheritAttrs: false,
-});
+})
 
-const id = useId();
-const contentRef = ref<HTMLParagraphElement | null>();
-const tooltipText = ref("");
+const contentRef = useTemplateRef('content')
+const tooltipText = ref('')
 
 onMounted(() => {
-  if(contentRef.value) {
-    tooltipText.value = contentRef.value.textContent || "";
+  if (contentRef.value) {
+    tooltipText.value = contentRef.value.textContent || ''
   }
-});
+})
 </script>
+
 <style scoped>
 p {
   line-height: var(--lh) !important;
