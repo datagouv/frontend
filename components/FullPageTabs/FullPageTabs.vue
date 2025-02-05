95f375d97890a4a4f06a6029bdedc858e85927cf
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2025-02-05 14:34:55 +0100

    fix: wrong arbitrary value

--- components/FullPageTabs/FullPageTabs.vue
+++ components/FullPageTabs/FullPageTabs.vue
@@ -5,7 +5,7 @@
         <NuxtLinkLocale
           v-for="link in links"
           :key="link.label"
-          class="bg-blue-action-low px-4 py-2 !bg-none font-bold aria-current-page:text-datagouv-dark aria-current-page:border-t-2 aria-current-page:border-t-datagouv-dark aria-current-page:bg-white aria-current-page:border-x aria-current-page:border-x-gray-default aria-current-page:shadow-[0 1px white] hover:aria-current-page:!bg-white active::aria-current-page:!bg-white hover:!bg-blue-action-low-hover active:!bg-blue-action-low-active"
+          class="bg-blue-action-low px-4 py-2 !bg-none font-bold aria-current-page:text-datagouv-dark aria-current-page:border-t-2 aria-current-page:border-t-datagouv-dark aria-current-page:bg-white aria-current-page:border-x aria-current-page:border-x-gray-default aria-current-page:shadow-[0_1px_white] hover:aria-current-page:!bg-white active::aria-current-page:!bg-white hover:!bg-blue-action-low-hover active:!bg-blue-action-low-active"
           :to="link.href"
           :aria-current="isCurrent(link.href) ? 'page': false"
         >
