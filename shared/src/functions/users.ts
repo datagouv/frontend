5251f92036eb0a58510dd6d49573967f83d498fb
Thibaud Dauce <git@hadibut.fr>
2025-02-27 15:30:37 +0100

    Fix wrong url for avatars

--- shared/src/functions/users.ts
+++ shared/src/functions/users.ts
@@ -3,5 +3,5 @@ import { useComponentsConfig } from '../main'
 
 export function getUserAvatar(user: User, size: number) {
   const config = useComponentsConfig()
-  return user.avatar_thumbnail || `${config.apiBase}/avatars/${user.id}/${size}`
+  return user.avatar_thumbnail || `${config.apiBase}/api/1/avatars/${user.id}/${size}`
 }
